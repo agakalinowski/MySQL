a087c9c8f5152e2bd7e21e2e7d01f24d120d7178
agakalinowski <144943997+agakalinowski@users.noreply.github.com>
2023-11-19 19:43:33 +0100

    Create Query_3.sql

---
+++ NEW!/Query_3.sql
@@ -0,0 +1,14 @@
+**Query 3**:
+    SELECT c.category_name, COUNT(*) AS product_count
+    FROM sql_product_categories c
+    JOIN sql_product p ON p.product_id = c.product_id
+    GROUP BY c.category_name
+
+**[Results][4]**:
+
+    | category_name | product_count |
+    |---------------|---------------|
+    |   Electronics |             2 |
+    |    Home Goods |             1 |
+    |    Stationery |             2 |
+[4]: http://sqlfiddle.com/#!9/574705/3/2
